29f703d9b74e7a17c606411b9ddd80479b2f6a7c
zuhair <zuhair.sakharkar@fatakpay.com>
2024-10-08 20:23:38 +0530

    Search Bar Creation

--- src/components/Navbars/LoggedInNavbar.tsx
+++ src/components/Navbars/LoggedInNavbar.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import { IoMdArrowForward, IoMdArrowBack, IoMdTime } from 'react-icons/io'
 
 const LoggedInNavbar = () => {
   return (
-    <div>LoggedInNavbar</div>
+    <div className="w-full flex items-center m-auto bg-primary-dark p-2">
+      <div className="flex items-center justify-end gap-1 w-[500px]">
+        <IconContainer navigation icon={<IoMdArrowBack size={22} />} />
+        <IconContainer navigation icon={<IoMdArrowForward size={22} />} />
+        <IconContainer icon={<IoMdTime size={22} />} />
+      </div>
+      <div>
+        <SearchBar />
+      </div>
+    </div>
+  )
+}
+
+const IconContainer = ({
+  icon,
+  navigation,
+}: {
+  icon: React.ReactNode
+  navigation?: boolean
+}) => {
+  return (
+    <div
+      className={`p-1 cursor-pointer rounded ${navigation ? 'text-white/60' : 'text-white hover:bg-primary-light/60'}`}
+    >
+      {icon}
+    </div>
+  )
+}
+
+const SearchBar = () => {
+  return (
+    <div className='flex w-[600px] justify-between items-center text-white'>
+      <input type="text" placeholder='search' className='bg-transparent outline-none placeholder:text-white placeholder:text-sm' />
+      <div>Search</div>
+    </div>
   )
 }
 
